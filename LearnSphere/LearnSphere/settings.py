c78709073ae6177784d2a096d263c859ae5e33d9
typicaleoxx <lsneha991@gmail.com>
2024-03-10 13:56:22 +0545

    tried using sql but later decided to go with default mysqli to solely focus on other important aspects and remove distraction, so commented sql database setting

--- LearnSphere/LearnSphere/settings.py
+++ LearnSphere/LearnSphere/settings.py
@@ -80,13 +80,22 @@ WSGI_APPLICATION = "LearnSphere.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+"""DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "LearnSphere",
+        "USER": "root",
+        "PASSWORD": "password",
+        "HOST": "localhost",  # Or the hostname where your MySQL server is running
+        "PORT": "3306",  # Default MySQL port
+    }
+}"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
